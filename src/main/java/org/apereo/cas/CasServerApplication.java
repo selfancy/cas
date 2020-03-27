17901f6bbbaa31bd930c37a9301faa25815f44c3
mike <liuqiao@haofeigame.com>
2020-03-27 15:21:12 +0800

    add jdbc support

--- src/main/java/org/apereo/cas/CasServerApplication.java
+++ src/main/java/org/apereo/cas/CasServerApplication.java
@@ -3,6 +3,8 @@ package org.apereo.cas;
 import org.apereo.cas.web.CasWebApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 /**
  * Created by mike on 2020/3/26 since 1.0
@@ -10,9 +12,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class CasServerApplication {
 
-    public static void main(String[] args) {
-        new SpringApplicationBuilder()
-                .sources(CasWebApplication.class)
+    public static void main(String[] args) throws Exception {
+        new SpringApplicationBuilder(CasWebApplication.class)
+                .properties(PropertiesLoaderUtils.loadProperties(new ClassPathResource("jdbc.properties")))
                 .run(args);
     }
 }
